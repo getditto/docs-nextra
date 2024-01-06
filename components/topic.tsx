83fff12f3ce7680651f851b3ab18851d1956aec0
Skyler Jokiel <sky.jokiel@outlook.com>
2024-01-05 16:16:11 -0800

    making it better

--- components/topic.tsx
+++ components/topic.tsx
@@ -3,3 +3,8 @@ import {Topic as getTopic} from "../topics"
 export const GetTopic = ({id}: {id:string}) => {
     return <span>{getTopic[id]}</span>
 }
+
+
+export const SomeText = ({id}: {id:string}) => {
+    return <span>text</span>
+}
--- topics/index.ts
+++ topics/index.ts
@@ -1,10 +1,37 @@
 import general from "./general.json";
-import mesh from "./mesh.json";
 import branding from "./branding.json";
 
-export type Topic = keyof typeof Topic;
-export const Topic = {
+/**
+ * Structure of a topic.
+ * 
+ * Note: This cannot be enforced on import of the json file but can be typed using typescript. Any non-standard
+ * fields are accessible via duck-typing but shouldn't be used.
+ * 
+ * TODO: Make tags strongly typed.
+ */
+interface TopicBody {
+    text: string;
+    tags: string[];
+}
+
+/**
+ * Topics allows us to have a strongly typed interface to our different json stored topics.
+ * Topics can only be accessed through Topic below using the
+ * 
+ * Topic('topic-id') syntax in mdx/tsx/ts files
+ */
+type Topics = {
+    [x: string]: TopicBody;
+}
+const Topics: Topics = {
     ...general,
-    ...mesh,
     ...branding
 };
+
+/**
+ * The method to use to get text from a json topic
+ */
+export type Topic = keyof typeof Topics;
+export const Topic = (id:Topic): string => {
+    return Topics[id].text;
+}
