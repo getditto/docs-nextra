ebb2d3553d9f6b175176e446ac4eba0dabeb8000
Manuel Garcia Genta <man.mdp@gmail.com>
2024-02-16 13:53:26 -0300

    render navigation tree

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -15,6 +15,14 @@ const Navigation = ({ pageMap, route }: NavigationProps) => {
   const [isLoaded, setIsLoaded] = useState(false)
   const languageIds = new Set(languagesData.map((lang) => lang.id))
 
+  const languageFolders = pageMap.filter(
+    (item) => item.kind === "Folder" && languageIds.has(item.name)
+  )
+  const otherFolders = pageMap.filter(
+    (item) => item.kind === "Folder" && !languageIds.has(item.name)
+  )
+  const pages = pageMap.filter((item) => item.kind === "MdxPage")
+
   useEffect(() => {
     if (language !== undefined) {
       setIsLoaded(true)
@@ -31,46 +39,53 @@ const Navigation = ({ pageMap, route }: NavigationProps) => {
     </Link>
   )
 
-  const languageFolders = pageMap.filter(
-    (item) => item.kind === "Folder" && languageIds.has(item.name)
-  )
-  const otherFolders = pageMap.filter(
-    (item) => item.kind === "Folder" && !languageIds.has(item.name)
-  )
-  const pages = pageMap.filter((item) => item.kind === "MdxPage")
+  const Folder = ({ folder, isLanguageFolder = false }) => {
+    if (isLanguageFolder && folder.name !== language) {
+      return null
+    }
+
+    return (
+      <div key={folder.name} className={s.folder}>
+        {folder.children?.map((child) => {
+          if (child.kind === "MdxPage") {
+            return <Page page={child} />
+          } else if (child.kind === "Folder") {
+            return <Folder folder={child} />
+          }
+          return null
+        })}
+      </div>
+    )
+  }
+
+  const Page = ({ page }) => renderLink(page, page.route === route)
 
   return (
     <>
       {isLoaded && (
         <nav className={s.nav}>
           {pages.length > 0 && (
-            <ul>
-              {pages.map((item) => renderLink(item, item.route === route))}
-            </ul>
+            <div>
+              {pages.map((page) => (
+                <Page page={page} />
+              ))}
+            </div>
           )}
 
           {otherFolders.length > 0 && (
-            <ul>
-              {otherFolders.map((item) =>
-                item.children?.map(
-                  (child) => child.kind === "MdxPage" && renderLink(child)
-                )
-              )}
-            </ul>
+            <div>
+              {otherFolders.map((folder) => (
+                <Folder folder={folder} />
+              ))}
+            </div>
           )}
 
           {languageFolders.length > 0 && (
-            <ul>
-              {languageFolders.map(
-                (item) =>
-                  item.name === language &&
-                  item.children?.map(
-                    (child) =>
-                      child.kind === "MdxPage" &&
-                      renderLink(child, child.route === route)
-                  )
-              )}
-            </ul>
+            <div>
+              {languageFolders.map((folder) => (
+                <Folder folder={folder} isLanguageFolder={true} />
+              ))}
+            </div>
           )}
         </nav>
       )}
