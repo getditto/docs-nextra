867ba3b18ec21964c77fe0925edf9842ba2924d5
Manuel Garcia Genta <man.mdp@gmail.com>
2024-02-21 15:26:50 -0300

    improve nav code

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from "nextra-theme-docs"
 import languagesData from "../language/languages"
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useState, useMemo } from "react"
 import LanguageContext from "../language/context"
 
 import s from "./navigation.module.css"
@@ -10,55 +10,71 @@ type NavigationProps = {
   route: any
 }
 
+const renderLink = (item, active = false) => (
+  <Link
+    key={item.name}
+    href={item.route}
+    className={`${s.link} ${active && s.active}`}
+  >
+    {item.name}
+  </Link>
+)
+
+const Folder = ({ folder, isLanguageFolder = false, language, route }) => {
+  if (isLanguageFolder && folder.name !== language) {
+    return null
+  }
+
+  return (
+    <div key={folder.name} className={s.folder}>
+      {folder.children?.map((child) => {
+        if (child.kind === "MdxPage") {
+          return <Page page={child} route={route} />
+        } else if (child.kind === "Folder") {
+          return <Folder folder={child} language={language} route={route} />
+        }
+        return null
+      })}
+    </div>
+  )
+}
+
+const Page = ({ page, route }) => renderLink(page, page.route === route)
+
 const Navigation = ({ pageMap, route }: NavigationProps) => {
   const { language } = useContext(LanguageContext)
   const [isLoaded, setIsLoaded] = useState(false)
-  const languageIds = new Set(languagesData.map((lang) => lang.id))
+  const languageIds = useMemo(
+    () => new Set(languagesData.map((lang) => lang.id)),
+    []
+  )
 
-  const languageFolders = pageMap.filter(
-    (item) => item.kind === "Folder" && languageIds.has(item.name)
+  const languageFolders = useMemo(
+    () =>
+      pageMap.filter(
+        (item) => item.kind === "Folder" && languageIds.has(item.name)
+      ),
+    [pageMap, languageIds]
   )
-  const otherFolders = pageMap.filter(
-    (item) => item.kind === "Folder" && !languageIds.has(item.name)
+
+  const otherFolders = useMemo(
+    () =>
+      pageMap.filter(
+        (item) => item.kind === "Folder" && !languageIds.has(item.name)
+      ),
+    [pageMap, languageIds]
   )
-  const pages = pageMap.filter((item) => item.kind === "MdxPage")
+
+  const pages = useMemo(
+    () => pageMap.filter((item) => item.kind === "MdxPage"),
+    [pageMap]
+  )
 
   useEffect(() => {
     if (language !== undefined) {
       setIsLoaded(true)
     }
   }, [language])
-
-  const renderLink = (item, active = false) => (
-    <Link
-      key={item.name}
-      href={item.route}
-      className={`${s.link} ${active && s.active}`}
-    >
-      {item.name}
-    </Link>
-  )
-
-  const Folder = ({ folder, isLanguageFolder = false }) => {
-    if (isLanguageFolder && folder.name !== language) {
-      return null
-    }
-
-    return (
-      <div key={folder.name} className={s.folder}>
-        {folder.children?.map((child) => {
-          if (child.kind === "MdxPage") {
-            return <Page page={child} />
-          } else if (child.kind === "Folder") {
-            return <Folder folder={child} />
-          }
-          return null
-        })}
-      </div>
-    )
-  }
-
-  const Page = ({ page }) => renderLink(page, page.route === route)
 
   return (
     <>
@@ -67,7 +83,7 @@ const Navigation = ({ pageMap, route }: NavigationProps) => {
           {pages.length > 0 && (
             <div>
               {pages.map((page) => (
-                <Page page={page} />
+                <Page page={page} route={route} />
               ))}
             </div>
           )}
@@ -75,7 +91,7 @@ const Navigation = ({ pageMap, route }: NavigationProps) => {
           {otherFolders.length > 0 && (
             <div>
               {otherFolders.map((folder) => (
-                <Folder folder={folder} />
+                <Folder folder={folder} language={language} route={route} />
               ))}
             </div>
           )}
@@ -83,7 +99,12 @@ const Navigation = ({ pageMap, route }: NavigationProps) => {
           {languageFolders.length > 0 && (
             <div>
               {languageFolders.map((folder) => (
-                <Folder folder={folder} isLanguageFolder={true} />
+                <Folder
+                  folder={folder}
+                  isLanguageFolder={true}
+                  language={language}
+                  route={route}
+                />
               ))}
             </div>
           )}
